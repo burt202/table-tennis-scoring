5cbc3f0bff680bae345c7bc39cda1d00e0fe56f1
Aaron Burtnyk <burt202@gmail.com>
2016-05-16 18:58:43 +0100

    Calculate player breakdowns

--- src/format-results.js
+++ src/format-results.js
@@ -74,9 +74,42 @@ function getAllResults (results) {
   )(results);
 }
 
+function getPlayerBreakdowns (players, results) {
+  var base = R.reduce(function (acc, val) {
+    acc[val] = {yetToPlay: R.without([val], players), results: []};
+    return acc;
+  }, {}, players);
+
+  return R.pipe(
+    R.reduce(function (acc, val) {
+      var resultParts = R.split(",", val);
+      var winner = resultParts[0];
+      var loser = resultParts[1];
+      var score = resultParts[2];
+
+      acc[winner] = R.evolve({
+        results: R.append({winner: winner, loser: loser, score: score}),
+        yetToPlay: R.without([loser])
+      }, acc[winner]);
+
+      acc[loser] = R.evolve({
+        results: R.append({winner: winner, loser: loser, score: score}),
+        yetToPlay: R.without([winner])
+      }, acc[loser]);
+
+      return acc;
+    }, base),
+    R.toPairs,
+    R.map(function (pair) {
+      return R.merge(pair[1], {name: pair[0]});
+    })
+  )(results);
+}
+
 module.exports = function (players, results) {
   return {
     standings: getStandings(players, results),
-    allResults: getAllResults(results)
+    allResults: getAllResults(results),
+    playerBreakdowns: getPlayerBreakdowns(players, results)
   };
 }
